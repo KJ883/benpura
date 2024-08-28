d18a45a07a877ca5eac33fc6000c3e3d8ef47fbd
nonpoly1192 <papiyom89@gmail.com>
2024-08-28 23:40:51 +0900

    [KJ]

--- src/main/java/com/example/demo/service/SettlementServiceImpl.java
+++ src/main/java/com/example/demo/service/SettlementServiceImpl.java
@@ -2,11 +2,13 @@ package com.example.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.entity.Orders;
 import com.example.demo.repository.CalendarRepository;
 import com.example.demo.repository.OrdersRepository;
 @Service
+@Transactional
 public class SettlementServiceImpl implements SettlementService {
 	@Autowired
 	OrdersRepository orderRepository;
